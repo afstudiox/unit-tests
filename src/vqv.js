4e608fa65f0e9bd74fb52c785e77335d9f7271e2
Andre Felipe <afelipes@gmail.com>
2022-01-17 16:34:07 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
 
+  if (typeof name === 'undefined' || typeof age === 'undefined') {
+    return undefined
+  }
+  let string = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return string;
+};
+  
 module.exports = vqv;
