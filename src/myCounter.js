33322e9308fbb5c62caa1fe4f74451750be5d66b
Andre Felipe <afelipes@gmail.com>
2022-01-21 00:36:13 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -13,18 +13,24 @@
   - Nenhum.
 
   Comportamento:
-  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  myCounter() // Retorna: [
+    0, 2, 3, 
+    1, 2, 3, 
+    2, 2, 3, 
+    3, 2, 3];
 */
 
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counterx = 2; counterx <= 3; counterx += 1) {
+      myArray.push(counterx);
     }
   }
   return myArray;
 };
+
+console.log(myCounter());
 
 module.exports = myCounter;
